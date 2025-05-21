83c5e7a0aee636a49b46d3de61213061df8c4efd
Othamae <vero.gconesa@gmail.com>
2025-05-21 21:32:57 +0200

    First commit

---
+++ frontend/src/App.ts
@@ -0,0 +1,10 @@
+export function App(): HTMLElement {
+  const container = document.createElement('div')
+
+  container.innerHTML = `
+		<h1>ft_transcendence</h1>
+		<p>by Eliska, Azeez and Vero</p>
+	`
+
+  return container
+}
