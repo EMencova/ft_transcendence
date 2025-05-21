c599bc5cc664a2b495bfca3c0c4fc0088587e3a5
Othamae <vero.gconesa@gmail.com>
2025-05-21 22:49:05 +0200

    Add tailwindcss

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,10 +1,18 @@
+import { Footer } from './Footer.ts'
+import { Header } from './Header.ts'
+
+
 export function App(): HTMLElement {
   const container = document.createElement('div')
+  container.className = 'flex flex-col h-screen'
 
-  container.innerHTML = `
-		<h1>ft_transcendence</h1>
-		<p>by Eliska, Azeez and Vero</p>
-	`
+  const main = document.createElement('main')
+  main.className = 'flex-grow p-4 text-xl mt-80'
+  main.innerHTML = `<p>Game PONG here</p>`
+
+  container.appendChild(Header())
+  container.appendChild(main)
+  container.appendChild(Footer());
 
   return container
 }
