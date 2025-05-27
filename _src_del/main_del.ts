88c630754a2275f9a8209490389e49b0850d17c2
AbdulAzeez Shobajo <digitalpoolng@gmail.com>
2025-05-27 20:58:08 +0100

    Delete _src_del directory

--- _src_del/main_del.ts
+++
@@ -1,9 +0,0 @@
-import { startGame } from "./game_del";
-
-window.onload = () => {
-  const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-  const context = canvas.getContext("2d");
-  if (canvas && context) {
-    startGame(canvas, context);
-  }
-};
