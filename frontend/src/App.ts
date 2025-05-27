5707172b73c840ea0019bb3af30a7355e1621bea
Othamae <vero.gconesa@gmail.com>
2025-05-27 20:23:04 +0200

    Add simple login/register

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,20 +1,46 @@
 import { Footer } from './Footer.ts'
 import { Header } from './Header.ts'
+import { LoginRegister } from './LoginRegister.ts'
 import { Pong } from './Pong.ts'
 
+// WITHOUT LOGIN/REGISTER
+// export function App(): HTMLElement {
+//   const container = document.createElement('div')
+//   container.className = 'flex flex-col h-screen w-full'
+
+//   container.appendChild(Header())
+//   container.appendChild(Pong())
+//   container.appendChild(Footer());
+
+//   return container
+// }
+
+let isLoggedIn = false
 
 export function App(): HTMLElement {
   const container = document.createElement('div')
-  container.className = 'flex flex-col h-screen w-full'
+  container.className = 'h-screen w-full'
 
-  const main = document.createElement('main')
-  main.className = 'flex-grow p-4 text-xl mt-80'
-  main.innerHTML = `<p>Game PONG here</p>`
+  function render() {
+    container.innerHTML = '' // Limpiar el contenido
 
-  container.appendChild(Header())
-  // container.appendChild(main)
-  container.appendChild(Pong())
-  container.appendChild(Footer());
+    if (!isLoggedIn) {
+      container.appendChild(LoginRegister(() => {
+        isLoggedIn = true
+        render()
+      }))
+    } else {
+      const main = document.createElement('div')
+      main.className = 'flex flex-col h-screen w-full'
 
+      main.appendChild(Header())
+      main.appendChild(Pong())
+      main.appendChild(Footer())
+
+      container.appendChild(main)
+    }
+  }
+
+  render()
   return container
 }
